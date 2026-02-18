c1bca7b57a6f5602b3b1116b8edca2920f24a309
ceciodev <lorenza61.cecilia01@gmail.com>
2026-02-18 12:18:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import os
+from flask import Flask
+
+app = Flask(__name__)
+
+# i tuoi endpoint qui
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 import json
 import random
 
@@ -134,3 +143,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
